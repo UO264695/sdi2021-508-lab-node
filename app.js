886af091f40b133be59ebc230e944fe42f360c43
UO264695 <uo264695@uniovi.es>
2021-04-01 21:49:40 +0200

    SDI-2021-508-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 // Variables
 app.set('port', 8081);
 app.set('db','mongodb://admin:sdimongo2021@tiendamusica-shard-00-00.lfucn.mongodb.net:27017,tiendamusica-shard-00-01.lfucn.mongodb.net:27017,tiendamusica-shard-00-02.lfucn.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mcachk-shard-0&authSource=admin&retryWrites=true&w=majority');
 
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 
 require("./routes/rautores.js")(app, swig);
 
